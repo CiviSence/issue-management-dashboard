a38ec60bd370b2e63cfdc75bc86dfe04b3061787
Vaibhav Raj Harsh <156331614+vaibhavrajharsh@users.noreply.github.com>
2026-03-23 11:50:43 +0530

    Update axios baseURL to include '/docs'

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -7,7 +7,7 @@ import {
 } from "./auth-utils";
 
 const instance = axios.create({
-  baseURL: "https://csm-backend-aws.duckdns.org/",
+  baseURL: "https://csm-backend-aws.duckdns.org/docs",
   withCredentials: true, // IMPORTANT for session/cookies
   headers: {
     Accept: "application/json",
